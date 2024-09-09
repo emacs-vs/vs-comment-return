78f45a1468cf7d465924af48bace5be6ccf72255
JenChieh <jcs090218@gmail.com>
2024-09-09 16:08:30 -0700

    Bump version

--- vs-comment-return.el
+++ vs-comment-return.el
@@ -5,7 +5,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-vs/vs-comment-return
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience
 
